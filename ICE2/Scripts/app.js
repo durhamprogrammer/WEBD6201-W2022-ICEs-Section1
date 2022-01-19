efe190fdac21a73e10faa88346df218b6b30f486
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-01-19 12:05:51 -0500

    added ICE 2 Project Files

---
+++ ICE2/Scripts/app.js
@@ -0,0 +1,14 @@
+// IIFE -- Immediately Invoked Function Express
+// AKA anonymous self-executing function
+(function()
+{
+
+    function Start()
+    {
+        console.log("App Started!!");
+    }
+
+    window.addEventListener("load", Start);
+
+
+})();
