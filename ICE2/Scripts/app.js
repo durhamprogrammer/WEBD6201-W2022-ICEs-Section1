4b7013fb441e6e743ef855cc2ccf64e7f7398008
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-01-21 17:04:57 -0500

    added DOM append example

--- ICE2/Scripts/app.js
+++ ICE2/Scripts/app.js
@@ -9,6 +9,20 @@
         {
             location.href = "about.html";
         });
+
+        // Step 1 - get a reference to an entry point(s) (insertion / deletion point)
+        let MainContent = document.getElementsByTagName("main")[0];
+        
+        // Step 2 - Create a HTML Element in memory
+        let MainParagraph = document.createElement("p");
+
+        // Step 3 - Configure new Element
+        MainParagraph.setAttribute("id", "MainParagraph");
+        MainParagraph.setAttribute("class", "mt-3");
+        MainParagraph.textContent = "This is the Main Paragraph!";
+
+        // Step 4 - perform insertion / deletion
+        MainContent.appendChild(MainParagraph);
     }
 
 
