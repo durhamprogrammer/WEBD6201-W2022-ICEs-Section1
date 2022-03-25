443d69355824b0336bcce3603e98d4957e5b8a66
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-03-25 16:47:07 -0400

    converted to ESM

--- Temp/app.js
+++ Temp/app.js
@@ -1,23 +1,28 @@
 "use strict";
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var app = express();
-app.set('views', path.join(__dirname, 'views'));
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_errors_1 = __importDefault(require("http-errors"));
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const morgan_1 = __importDefault(require("morgan"));
+const index_1 = __importDefault(require("./Routes/index"));
+const users_1 = __importDefault(require("./Routes/users"));
+const app = (0, express_1.default)();
+app.set('views', path_1.default.join(__dirname, 'Views'));
 app.set('view engine', 'ejs');
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use((0, morgan_1.default)('dev'));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use((0, cookie_parser_1.default)());
+app.use(express_1.default.static(path_1.default.join(__dirname, 'Client')));
+app.use(express_1.default.static(path_1.default.join(__dirname, 'node_modules')));
+app.use('/', index_1.default);
+app.use('/users', users_1.default);
 app.use(function (req, res, next) {
-    next(createError(404));
+    next((0, http_errors_1.default)(404));
 });
 app.use(function (err, req, res, next) {
     res.locals.message = err.message;
@@ -25,5 +30,5 @@ app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
-module.exports = app;
+exports.default = app;
 //# sourceMappingURL=app.js.map
