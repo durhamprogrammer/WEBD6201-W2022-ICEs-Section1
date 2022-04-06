6619a90a53d2dadef2209f2fab8b4b1aa412308c
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-04-06 12:07:06 -0400

    added project files for ICE12

---
+++ ICE12/Client/Scripts/app.ts
@@ -0,0 +1,307 @@
+// IIFE -- Immediately Invoked Function Express
+// AKA anonymous self-executing function
+
+"use strict";
+(function()
+{
+    function AuthGuard(): void
+    {
+        let protected_routes = [
+            "/contact-list",
+            "/edit"
+        ];
+    
+        if(protected_routes.indexOf(location.pathname) > -1)
+        {
+            // check if user is logged in
+            if(!sessionStorage.getItem("user"))
+            {
+                // if not...change the active link to "login"
+                location.href = "/login";
+            }
+        }
+    }
+
+    function DisplayHome(): void
+    {
+        console.log("Home Page");
+        $("#AboutUsButton").on("click", () => 
+        {
+            location.href = "/about";
+        });
+
+        $("main").append(`<p id="MainParagraph" class="mt-3">This is the Main Paragraph</p>`);
+
+        $("main").append(`
+        <article>
+            <p id="ArticleParagraph" class="mt-3">This is the Article Paragraph</p>
+            </article>`);
+    }
+
+    function DisplayAboutPage(): void
+    {
+        console.log("About Us Page");
+    }
+
+    function DisplayProjectsPage(): void
+    {
+        console.log("Our Projects Page");
+    }
+
+    function DisplayServicesPage(): void
+    {
+        console.log("Our Services Page");
+    }
+
+    /**
+     * Adds a Contact Object to localStorage
+     *
+     * @param {string} fullName
+     * @param {string} contactNumber
+     * @param {string} emailAddress
+     */
+    function AddContact(fullName: string, contactNumber: string, emailAddress: string)
+    {
+        let contact = new core.Contact(fullName, contactNumber, emailAddress);
+        if(contact.serialize())
+        {
+            let key = contact.FullName.substring(0, 1) + Date.now();
+                
+            localStorage.setItem(key, contact.serialize());
+        }
+    }
+
+    /**
+     * This method validates an input text field in the form and displays
+     * an error in the message area
+     *
+     * @param {string} input_field_ID
+     * @param {RegExp} regular_expression
+     * @param {string} error_message
+     */
+    function ValidateField(input_field_ID: string, regular_expression: RegExp, error_message: string)
+    {
+        let messageArea = $("#messageArea").hide();
+        
+        $("#" + input_field_ID).on("blur", function()
+        {
+            let inputFieldText: string = $(this).val() as string;
+
+            if(!regular_expression.test(inputFieldText))
+            {
+                $(this).trigger("focus").trigger("select"); 
+                messageArea.addClass("alert alert-danger").text(error_message).show(); 
+            }
+            else
+            {
+                messageArea.removeAttr("class").hide();
+            }
+        });
+    }
+
+    function ContactFormValidation()
+    {
+        ValidateField("fullName", /^([A-Z][a-z]{1,3}\.?\s)?([A-Z][a-z]{1,})+([\s,-]([A-Z][a-z]{1,}))*$/,"Please enter a valid Full Name.");
+        ValidateField("contactNumber", /^(\+\d{1,3}[\s-.])?\(?\d{3}\)?[\s-.]?\d{3}[\s-.]?\d{4}$/, "Please enter a valid Contact Number.");
+        ValidateField("emailAddress", /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,10}$/, "Please enter a valid Email Address.");
+    }
+
+    function DisplayContactPage()
+    {
+        console.log("Contact Us Page");
+
+        $("a[data='contact-list']").off("click");
+        $("a[data='contact-list']").on("click", function()
+        {
+            location.href = "/contact-list";
+        });
+
+        ContactFormValidation();
+        
+        let sendButton = document.getElementById("sendButton") as HTMLElement;
+        let subscribeCheckbox = document.getElementById("subscribeCheckbox") as HTMLInputElement;
+
+        sendButton.addEventListener("click", function()
+        {
+            if(subscribeCheckbox.checked)
+            { 
+                let fullName = document.forms[0].fullName.value as string;
+                let contactNumber = document.forms[0].contactNumber.value as string;
+                let emailAddress = document.forms[0].emailAddress.value as string;
+
+                AddContact(fullName, contactNumber, emailAddress);
+            }
+        });
+    }
+
+    function DisplayContactListPage()
+    {
+        console.log("Contact-List Page");
+
+        $("a.delete").on("click", function(event)
+        {
+            if(!confirm("Are you sure?"))
+            {
+                event.preventDefault();
+               // refresh after deleting
+                location.href = "/contact-list";
+            }
+        });
+    }
+
+    function DisplayEditPage()
+    {
+        console.log("Edit Page");
+
+        ContactFormValidation();
+    }
+
+    function CheckLogin()
+    {
+        // if user is logged in, then...
+        if(sessionStorage.getItem("user"))
+        {
+            // swap out the login link for logout
+            $("#login").html(
+                `<a id="logout" class="nav-link" href="#"><i class="fas fa-sign-out-alt"></i> Logout</a>`
+            );
+
+            $("#logout").on("click", function()
+            {
+                // perform logout
+                sessionStorage.clear();
+
+                // swap out the logout link for login
+                $("#login").html(
+                    `<a class="nav-link" href="/login"><i class="fas fa-sign-in-alt"></i> Login</a>`
+                );
+
+                // redirect back to login page
+                location.href = "/login";
+            });
+        }
+    }
+
+    function DisplayLoginPage()
+    {
+        console.log("Login Page");
+        let messageArea = $("#messageArea");
+        messageArea.hide();
+
+        $("#loginButton").on("click", function()
+        {
+            let success = false;
+
+            // create an empty user object
+            let newUser = new core.User();
+
+            let username = document.forms[0].username.value as string;
+            let password = document.forms[0].password.value as string;
+
+            // use jQuery shortcut to lod the users.json file
+            $.get("./Data/users.json", function(data)
+            {
+                // for every user in the users.json file, loop
+                for (const user of data.users) 
+                {
+                    // check if the username and password entered matches the user data
+                    if(username == user.Username && password == user.Password)
+                    {
+                        console.log("conditional passed!");
+                        // get the user data from the file and assign it to our empty user object
+                        newUser.fromJSON(user);
+                        success = true;
+                        break;
+                    }
+                }
+
+                 // if username and password matches..success! -> perform the login sequence
+                if(success)
+                {
+                    // add user to session storage
+                    sessionStorage.setItem("user", newUser.serialize() as string);
+
+                    // hide any error message
+                    messageArea.removeAttr("class").hide();
+
+                    // redirect the user to the secure area of the site - contact-list.html
+                    location.href = "/contact-list";
+                }
+                else
+                {
+                    // display an error message
+                    $("#username").trigger("focus").trigger("select");
+                    messageArea.addClass("alert alert-danger").text("Error: Invalid Login Credentials").show();
+                }
+            });
+        });
+
+        $("#cancelButton").on("click", function()
+        {
+            // clear the login form
+            document.forms[0].reset();
+
+            // return to the home page
+            location.href = "/home";
+        });
+    }
+
+    function DisplayRegisterPage()
+    {
+        console.log("Register Page");
+    }
+
+    function Display404()
+    {
+
+    }
+
+    function Start()
+    {
+        console.log("App Started!!");
+
+        let page_id = $("body")[0].getAttribute("id");
+
+        CheckLogin();
+
+        switch (page_id)
+        {
+          case "home": 
+            DisplayHome();
+            break;
+          case "about": 
+            DisplayAboutPage();
+            break;
+          case "projects":
+            DisplayProjectsPage();
+            break;
+          case "services":
+            DisplayServicesPage();
+            break;
+          case "contact-list": 
+            DisplayContactListPage();
+            break;
+          case "contact": 
+            DisplayContactPage();
+            break;
+          case "edit": 
+            DisplayEditPage();
+            break;
+        case "add": 
+            DisplayEditPage();
+            break;
+          case "login": 
+            DisplayLoginPage();
+            break;
+          case "register": 
+            DisplayRegisterPage();
+            break;
+          case "404": 
+            Display404();
+            break;
+        }
+    }
+    
+    window.addEventListener("load", Start);
+
+})();
